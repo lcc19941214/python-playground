63ef7bd166c1d5fae548ffd2dc30d9271368e0f7
lcc19941214 <245987239@qq.com>
2019-05-09 18:41:51 +0800

    feat: update character

--- src/start/character.py
+++ src/start/character.py
@@ -1,6 +1,8 @@
-# -*- coding: utf-8 -*-
+# coding=utf-8
 
+# Normal Input
 print 100
+print 3.1415926897
 print '100'
 print 'hello'
 print '你好'
@@ -19,9 +21,22 @@ print chr(65)
 print len('abc')    # 3
 print len(u'abc')   # 3
 print len(u'中文')  # 2
-print len('\xe4\xb8\xad\xe6\x96\x87') # 6
+print len('\xe4\xb8\xad\xe6\x96\x87')  # 6
 
 # encode & decode
-print 'abc'.encode('utf-8') # 'abc
+print 'abc'.encode('utf-8')  # 'abc
 print 'abc'.decode('utf-8')
-print u'中文'.encode('utf-8') # '\xe4\xb8\xad\xe6\x96\x87'
+print u'中文'.encode('utf-8')  # '\xe4\xb8\xad\xe6\x96\x87'
+
+# placeholder
+# %s string this will always work
+# %d digit 是否在数字前面padStart：%xnd x代表需要pad的字符，可以省略，默认补充空格；n代表字符总长度
+# %f float 小数点后位数： %.nf n 代表需要显示的长度，默认6个
+# %x 16 radix
+print 'hello %s' % 'world'
+print '%s is now %d year\'s old' % ('Lily', 25)
+print 'PI is start with %f(with default to 6 digit)' % (3.141592653897)
+print 'PI is start with %.10f(with up to 10 digit)' % (3.141592653897)
+print 'The constant E is start with %d' % (2.71828)
+print 'Success Rate: %s%%' % 7.5
+print 'My favorite movie is %03d' % 7 # 007
