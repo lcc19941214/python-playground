256d51c22418c48294bf2c352e5efab08e59141f
lcc19941214 <245987239@qq.com>
2019-05-17 13:34:32 +0800

    feat: add README.md

---
+++ README.md
@@ -0,0 +1,28 @@
+# Python Playground
+
+
+## basic
+
+- [character.py](./src/basic/character.py)
+- [condition.py](./src/basic/condition.py)
+- [first.py](./src/basic/first.py)
+- [loop.py](./src/basic/loop.py)
+- [variable.py](./src/basic/variable.py)
+
+### data-type
+
+- [dict.py](./src/basic/data-type/dict.py)
+- [list.py](./src/basic/data-type/list.py)
+- [set.py](./src/basic/data-type/set.py)
+- [string.py](./src/basic/data-type/string.py)
+- [tuple.py](./src/basic/data-type/tuple.py)
+
+### function
+
+- [call_function.py](./src/basic/function/call_function.py)
+- [define_function.py](./src/basic/function/define_function.py)
+- [parameter.py](./src/basic/function/parameter.py)
+
+## utils
+
+- [section.py](./src/utils/section.py)
---
+++ scripts/create-readme.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+root=$(pwd)
+README="$root/README.md"
+echo -e '# Python Playground\n' >$README
+
+function generate() {
+  local DIR=$1
+  local anchor=$2
+  local filenames=$(ls $DIR)
+  local files=()
+  local dirs=()
+
+  for filename in ${filenames[@]}; do
+    local dir="$DIR/$filename"
+    if [ $filename == '__init__.py' ]; then
+      continue
+    fi
+
+    if [ $filename == 'README.md' ]; then
+      echo "- [$filename]($dir)" >>$README
+      continue
+    fi
+
+    if [ -d $dir ]; then
+      dirs=(${dirs[@]} $filename)
+    elif [ -f $dir ]; then
+      files=(${files[@]} $filename)
+    fi
+  done
+
+  for file in ${files[@]}; do
+    local dir="$DIR/$file"
+    echo "- [$file]($dir)" >>$README
+  done
+
+  for file in ${dirs[@]}; do
+    local dir="$DIR/$file"
+    echo -e "\n$anchor $file\n" >>$README
+    generate "$dir" "$anchor#"
+  done
+}
+
+generate './src' '##'
+echo 'done'
